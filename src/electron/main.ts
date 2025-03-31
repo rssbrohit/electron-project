c3cfee76c7645f07296c2e4324315c5eb2128d12
Rohit Tandon <rssbrohit@gmail.com>
2025-03-31 17:40:14 +0530

    electron builder configured.

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,7 +1,7 @@
 import {app, BrowserWindow} from 'electron'
 import path from 'path'
 
-type test = string;
+// type test = string;
 app.on('ready', () => {
     const mainWindow = new BrowserWindow({});
 

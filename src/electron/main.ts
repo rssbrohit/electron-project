7fa59c36c4f004a334b25f45f8ff445fd78e1610
Rohit Tandon <rssbrohit@gmail.com>
2025-04-04 18:39:52 +0530

    communicating with the UI

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,10 +1,17 @@
-import {app, BrowserWindow} from 'electron'
+import {app, BrowserWindow, ipcMain} from 'electron'
 import path from 'path'
 import { isDev } from './util.js';
+import { getStaticData, pollResources } from './resourceManager.js';
+import { getPreloadPath } from './pathResolver.js';
 
 // type test = string;
 app.on('ready', () => {
-    const mainWindow = new BrowserWindow({});
+    const mainWindow = new BrowserWindow({
+        webPreferences: {
+            preload:getPreloadPath(),
+
+        },
+    });
 
     if(isDev())
     {
@@ -16,5 +23,11 @@ app.on('ready', () => {
         mainWindow.loadFile(path.join(app.getAppPath() , '/dist-react/index.html'));
 
     }
+
+    pollResources(mainWindow);
+
+    ipcMain.handle("getStaticData",()=>{
+        return getStaticData();
+    });
 
 })
--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,9 +1,18 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  //@ts-ignore
+  window.electron.getStaticData();
+
+  useEffect(()=>{
+    //@ts-ignore
+    window.electron.subscribeStatistics((stats) => console.log(stats) );
+  })
+
 
   return (
     <>

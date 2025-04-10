68e19f429184ae90f45795512eb45dd4ce07d524
Rohit Tandon <rssbrohit@gmail.com>
2025-04-10 16:14:43 +0530

    Creating and hiding tray elements.

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,8 +1,9 @@
-import {app, BrowserWindow, ipcMain} from 'electron'
+import {app, BrowserWindow, ipcMain, Tray} from 'electron'
 import path from 'path'
 import { ipcMainHandle, isDev } from './util.js';
 import { getStaticData, pollResources } from './resourceManager.js';
-import { getPreloadPath, getUIPath } from './pathResolver.js';
+import { getAssetPath, getPreloadPath, getUIPath } from './pathResolver.js';
+import { createTray } from './tray.js';
 
 // type test = string;
 app.on('ready', () => {
@@ -30,5 +31,30 @@ app.on('ready', () => {
     ipcMainHandle("getStaticData",()=>{
         return getStaticData();
     });
+    createTray(mainWindow);
+    handleCloseEvents(mainWindow);
 
-})
+});
+
+function handleCloseEvents(mainWindow: BrowserWindow) {
+    let willClose = false;
+  
+    mainWindow.on('close', (e) => {
+      if (willClose) {
+        return;
+      }
+      e.preventDefault();
+      mainWindow.hide();
+      if (app.dock) {
+        app.dock.hide();
+      }
+    });
+  
+    app.on('before-quit', () => {
+      willClose = true;
+    });
+  
+    mainWindow.on('show', () => {
+      willClose = false;
+    });
+  }
